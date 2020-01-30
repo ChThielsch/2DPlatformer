77e7a8ec7985a05c7749881e0dafc1fd610ad2b6
Christian <ChThielsch@users.noreply.github.com>
2020-01-30 15:27:30 +0100

    Added UI and Score

---
+++ Assets/Scripts/GemController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GemController : MonoBehaviour
+{
+    /// <summary>
+    /// Reference to the ScoreController script on the UI text
+    /// </summary>
+    private ScoreController _scoreController;
+
+    void Awake()
+    {
+        _scoreController = GameObject.Find("Text_Gems").GetComponent(typeof(ScoreController)) as ScoreController;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            // Check list because OnTriggerEnter sometimes triggers twice before object is disabled
+            if (!_scoreController.collectedGems.Contains(this.gameObject))
+            {
+                _scoreController.collectedGems.Add(this.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+}
