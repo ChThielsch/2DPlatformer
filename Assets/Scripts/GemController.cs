8f36d1e6c30fd89bb174b5883b5b854c0d2cd81f
Christian <ChThielsch@users.noreply.github.com>
2020-01-30 16:01:31 +0100

    Added Music and Pickup Sound

--- Assets/Scripts/GemController.cs
+++ Assets/Scripts/GemController.cs
@@ -7,22 +7,28 @@ public class GemController : MonoBehaviour
     /// </summary>
     private ScoreController _scoreController;
 
+    /// <summary>
+    /// Reference tp the AudioSource on the gameObject
+    /// </summary>
+    private AudioSource _audioSource;
+
     void Awake()
     {
         _scoreController = GameObject.Find("Text_Gems").GetComponent(typeof(ScoreController)) as ScoreController;
+        _audioSource = gameObject.GetComponent(typeof(AudioSource)) as AudioSource;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag=="Player")
+        if (collision.gameObject.tag == "Player")
         {
             // Check list because OnTriggerEnter sometimes triggers twice before object is disabled
             if (!_scoreController.collectedGems.Contains(this.gameObject))
             {
+                _audioSource.Play();
                 _scoreController.collectedGems.Add(this.gameObject);
             }
             Destroy(this.gameObject);
         }
     }
-
 }
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GameObject> collectedGems;
+
+    /// <summary>
+    /// Reference to the text component 
+    /// </summary>
+    private TMP_Text _textComponent;
+
+    private int _prevousScore = -1;
+    private int _maxScore = 0;
+
+    void Awake()
+    {
+        _textComponent = gameObject.GetComponent<TMP_Text>();
+        _maxScore = GameObject.FindGameObjectsWithTag("Pickup").Length;
+    }
+
+    void Update()
+    {
+        if (collectedGems.Count > _prevousScore)
+        {
+            _textComponent.text = collectedGems.Count + "/" + _maxScore;
+            _prevousScore = collectedGems.Count;
+        }
+    }
+}
